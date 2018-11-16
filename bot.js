d70b703f2beda5725496f672fa68880b06da8589
AMr404 <36173199+AMr404@users.noreply.github.com>
2018-11-17 00:39:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,27 @@
-var ServerID = "489538820795006976"; 
-var ChannelID = "489538820795006978";
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('warn', console.warn);
 
-client.on('error', console.error);
+client.on('ready', () => {
+console.log("hehe");
+});
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(`
 
+Resident Roleplay Community MTA
 
-client.on('ready', () => console.log('Starting'));
+https://discord.gg/dp7rKQd
 
-client.on('disconnect', () => console.log('disconnected!'));
+- ريسندت، خادم بسيط
+كل ما ننوي هو تحسين فكرة لعب الأدوار عند الجميع، مع قواعد جديده تحسن من لعب الأدوار واتقانه أكثر،
+- مع أداره لها خبره وزمن في لعب الأدوار
 
-client.on('reconnecting', () => console.log('reconnecting...'));
+`)
 
+}).catch(console.error)
+})
 
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
 
 client.login(process.env.BOT_TOKEN);
